ad74771a48934f8fd5064ad6fd797c47eb62eaa8
cb1690015395 <cb1690015395@qq.com>
2026-05-19 19:19:00 +0800

    chore: bump version to 1.0.6 merge bugfix release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.duoji.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.5-bugfix-batch"
+        versionCode = 7
+        versionName = "1.0.6-merge-bugfix"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
